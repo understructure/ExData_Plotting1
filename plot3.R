4052545bb481ef1fd4b7060dd10bc6661237e716
Matthew Cloney <consulting@understructure.com>
2015-04-13 16:35:37 -0700

    Updated r file

--- plot3.R
+++ plot3.R
@@ -39,7 +39,7 @@ axis(2, at=c(0, 10, 20, 30), labels=c("0", "10", "20", "30"))
 #?legend
 legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
        lty=c(1,1), # gives the legend appropriate symbols (lines)
-       lwd=c(1,1),col=c("black", "red","blue"), bty="n"
+       lwd=c(1,1),col=c("black", "red","blue") #, bty="n"
       #,cex=.7
 ) # gives the legend lines the correct color and width
 dev.off()
